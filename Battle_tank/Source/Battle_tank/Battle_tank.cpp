46f90af6376852147746d4c9bfc2b9aac80c1da0
kotsur <vitaly.kotsur@gmail.com>
2019-01-26 19:49:10 +0200

    Init UE project

---
+++ Battle_tank/Source/Battle_tank/Battle_tank.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battle_tank.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Battle_tank, "Battle_tank" );
---
+++ Battle_tank/Source/Battle_tank/Battle_tankGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Battle_tankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANK_API ABattle_tankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
